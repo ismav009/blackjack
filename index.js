62257c8b2c9c4b68a549722fb3e369c233ae40e9
ismav009 <vamsi66666v@gmail.com>
2025-04-01 10:38:39 +0100

    index.js

--- index.js
+++ index.js
@@ -1,25 +1,62 @@
-let firstcard = 3
-let secondcard = 4
-function getRandomIntInclusive(min, max) {
-   return Math.floor(Math.random() * (max - min + 1)) + min;
-}
 
-let sum = firstcard + secondcard
+
+//function getRandomIntInclusive(min, max) {
+  // return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
+
+let cards = []
+let sum = 0
 
 let hasblackjack = false
-let alive = true
+let alive = false
+
+let player = {
+     name : "jack",
+     chips : 145
+
+}
+
+
+let playerEL = document.getElementById("player-el")
+playerEL.textContent = player.name + ": $" + player.chips
 
 let message = " "
 let messageEl = document.getElementById("message-el")
 // console.log(messageEl)
-
 let sumEl = document.querySelector(".sum-el")
 let cardsEl = document.querySelector(".cards-el")
 
+
+function getRandomCard() {
+    rnum = meth.floor(math.random() * 13) + 1
+    if(rnum === 1) {
+        return 11
+    } else if (rnum > 10) {
+        return 10
+    } else {
+        return rnum
+    }
+}
+
 function startGame() {
-   
-cardsEl.textContent = "Cards: " + firstcard + " " + secondcard
-if (sum <= 20) {
+    alive = true
+    let firstcard = getRandomCard()
+    let secondcard = getRandomCard()
+    cards = [firstcard, secondcard]
+    sum = firstcard + secondcard
+
+   renderGame()
+}
+
+function renderGame() {
+    cardsEL.textContent = "cards: "
+   for (let i = 0; i < cards.length; i++) {
+
+       cardsEl.textContent += cards[i] + " "
+   }
+
+sumEl.textContent = "Sum: " + sum
+if (sum <= 20) { 
    message = "Do you want to draw a new card? Your cards add up to " + sum
 
    
@@ -31,14 +68,21 @@ if (sum <= 20) {
    alive = false
 }
 messageEl.textContent = message
-sumEl.textContent = "Sum: " + sum
+
 
 }
 
-let newcardEL = document.querySelector(".newcard-btn")
+// let newcardEL = document.querySelector(".newcard-btn")
+
+
+
 function newCard() {
-  message = "Your new card is " + getRandomIntInclusive(2, 11)
-  console.log(message)
+    if (alive === true && hasblackjack === false) {
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        renderGame()
+    }
 
 }
 
